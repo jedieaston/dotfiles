1ed835390b02eb7d3c3ed8230de6fa7f1710cc57
Easton Pillay <easton@planeteaston.com>
2020-09-17 16:13:00 -0500

    no more upgrades.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 
 sudo apt-get update
-sudo apt-get upgrade -y
+# sudo apt-get upgrade -y
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 sudo apt-get install fortune cowsay -y
 cp .bashrc ~/.bashrc
