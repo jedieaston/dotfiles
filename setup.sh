845fd8ca2cde00a612b29c1c8e55832d2655087d
Easton Pillay <easton@planeteaston.com>
2020-09-17 16:09:34 -0500

    first commit!!1!

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sudo apt-get update
+sudo apt-get upgrade -y
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
+sudo apt-get install fortune cowsay -y
+cp .bashrc ~/.bashrc
+
