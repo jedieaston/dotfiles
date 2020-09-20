2b197d535d6042590ae07a159796213bf8159693
Easton Pillay <easton@planeteaston.com>
2020-09-20 14:34:50 -0500

    Added the Heroku CLI.
    urgh whyyyy

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 
 sudo apt-get update
 # sudo apt-get upgrade -y
+curl https://cli-assets.heroku.com/install.sh | sudo sh
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 sudo apt-get install fortune fortunes cowsay -y
 cp .bashrc ~/.bashrc
